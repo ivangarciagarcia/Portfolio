8d2cbac3a474df7c84bbda1aa79d8f1d65ca372b
Iván García García <garciaivan200@gmail.com>
2024-01-20 02:23:52 +0100

    component ProjectCard

--- portfolio/src/components/projectCard/ProjectCard.jsx
+++ portfolio/src/components/projectCard/ProjectCard.jsx
@@ -1,16 +1,19 @@
 import "./projectCard.css";
 
-export const ProjectCard = (props) => {
-
-  const { src, alt, title, description, buttonText, onClick } = props;
+export const ProjectCard = ({ imageUrl, title, description, githubLink, websiteLink }) => {
 
   return (
     <div className="card">
-      <img src={src} alt={alt} />
+      <img src={process.env.PUBLIC_URL + imageUrl} alt={title} />
       <div className="card-content">
         <h2>{title}</h2>
-        <p>{description}</p>
-        <button onClick={onClick}>{buttonText}</button>
+        <p className="project-description">{description}</p>
+        <div className="card-links">
+          <a href={githubLink} target="_blank" rel="noopener noreferrer" className="link-button">Ver en GitHub</a>
+          {websiteLink && (
+            <a href={websiteLink} target="_blank" rel="noopener noreferrer" className="link-button">Visitar Sitio Web</a>
+          )}
+        </div>
       </div>
     </div>
   );
